c5f0f61704f2e02d042b7c6f1039436d25a9799b
Tamirys Hariadny Camargo Couto <tamirys.couto@escola.pr.gov.br>
2026-06-23 08:57:16 -0300

    11

---
+++ script.js
@@ -0,0 +1,35 @@
+// ANIMAÇÃO AO ROLAR (fade-in)
+const sections = document.querySelectorAll("section");
+
+function showSections() {
+    const triggerBottom = window.innerHeight * 0.85;
+
+    sections.forEach(section => {
+        const sectionTop = section.getBoundingClientRect().top;
+
+        if (sectionTop < triggerBottom) {
+            section.classList.add("show");
+        }
+    });
+}
+
+window.addEventListener("scroll", showSections);
+showSections();
+
+// BOTÃO VOLTAR AO TOPO
+const topBtn = document.getElementById("topBtn");
+
+window.addEventListener("scroll", () => {
+    if (window.scrollY > 300) {
+        topBtn.style.display = "block";
+    } else {
+        topBtn.style.display = "none";
+    }
+});
+
+topBtn.addEventListener("click", () => {
+    window.scrollTo({
+        top: 0,
+        behavior: "smooth"
+    });
+});
